b36ed6bb321e233c70374220e6b6a178752f9909
timpan42 <tim@fagerdal.se>
2024-06-25 14:53:20 +0200

    added smoothe movement to the click move

--- project-blank/Assets/Script/Movement.cs
+++ project-blank/Assets/Script/Movement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,8 +8,10 @@ public class Movement : MonoBehaviour
 {
     private Vector2 moveDirection;
     [SerializeField] private int walkLength = 1;
+    [SerializeField] private int speed = 1;
     [SerializeField] private Vector3 offset;
-
+    private bool isMoveToOn = false;
+    private Coroutine corMoveTo;
 
     public void arrowWalk(InputAction moveInput)
     {
@@ -18,7 +21,34 @@ public class Movement : MonoBehaviour
 
     public void mouseMove(Vector3Int mousePosition)
     {
-        transform.position = mousePosition + offset;
+        if (!isMoveToOn)
+        {
+            corMoveTo = StartCoroutine(moveTo(mousePosition + offset));
+        }
+        else
+        {
+            StopCoroutine(corMoveTo);
+            corMoveTo = StartCoroutine(moveTo(mousePosition + offset));
+        }
+
+    }
+
+    private IEnumerator moveTo(Vector3 endPoint)
+    {
+        isMoveToOn = true;
+        while (transform.position != endPoint)
+        {
+            float step = speed * Time.deltaTime;
+            if (transform.position.y != endPoint.y)
+            {
+                step = step / 2;
+            }
+            Debug.Log(step);
+            transform.position = Vector3.MoveTowards(transform.position, endPoint, step);
+            yield return new WaitForEndOfFrame();
+        }
+        isMoveToOn = false;
+        yield return null;
     }
 
 }
